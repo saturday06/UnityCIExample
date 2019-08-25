b7327ff5b53d432a9bbb2bdcba9473d3fd84ba72
Isamu Mogi <isamu@leafytree.jp>
2019-08-26 02:58:17 +0900

    wip

---
+++ Assets/Editor/Builder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+namespace UnityCIExample
+{
+    public static class Builder
+    {
+        public static void BuildPlayer()
+        {
+            var buildPlayerOptions = new BuildPlayerOptions();
+            var report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            if (report.summary.result != BuildResult.Succeeded)
+            {
+                throw new Exception("Build failed: " + report.summary);
+            }
+        }
+    }
+}
